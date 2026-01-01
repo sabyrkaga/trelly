755c6de7fd4ab5000063d1140ff7e076d6a48741
Sabyrka <sabyrkaga@gmail.com>
2026-01-01 07:14:29 +0500

    feat: enhance task display with additional details and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 export const App = () => {
   const tasks = [
-    { id: 1, title: 'Купить продукты на неделю', isDone: false },
-    { id: 2, title: 'Полить цветы', isDone: true },
-    { id: 3, title: 'Сходить на тренировку', isDone: false },
+    {
+      id: 1,
+      title: 'Купить продукты на неделю',
+      isDone: false,
+      addedAt: '1 сентября',
+    },
+    {
+      id: 2,
+      title: 'Полить цветы',
+      isDone: true,
+      addedAt: '2 сентября',
+    },
+    {
+      id: 3,
+      title: 'Сходить на тренировку',
+      isDone: false,
+      addedAt: '3 сентября',
+    },
   ]
 
   return (
@@ -11,8 +26,18 @@ export const App = () => {
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            <div>{task.title}</div>
-            <input type="checkbox" checked={task.isDone} readOnly />
+            <div>
+              <strong>Заголовок: </strong>
+              <span>{task.title}</span>
+            </div>
+            <div>
+              <strong>Статус: </strong>
+              <input type="checkbox" checked={task.isDone} readOnly />
+            </div>
+            <div>
+              <strong>Дата создания задачи: </strong>
+              <span>{task.addedAt}</span>
+            </div>
           </li>
         ))}
       </ul>
