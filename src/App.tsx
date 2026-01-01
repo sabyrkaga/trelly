9eb3c6cbb0ecb2b1661b674bb22a2d410aa722a0
Sabyrka <sabyrkaga@gmail.com>
2026-01-01 07:21:51 +0500

    feat: add task priorities and improve loading states in task list

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,78 @@ export const App = () => {
       title: 'Купить продукты на неделю',
       isDone: false,
       addedAt: '1 сентября',
+      priority: 2,
     },
     {
       id: 2,
       title: 'Полить цветы',
       isDone: true,
       addedAt: '2 сентября',
+      priority: 0,
     },
     {
       id: 3,
       title: 'Сходить на тренировку',
       isDone: false,
       addedAt: '3 сентября',
+      priority: 1,
+    },
+    {
+      id: 4,
+      title: 'Срочно отправить рабочий отчет',
+      isDone: false,
+      addedAt: '4 сентября',
+      priority: 4,
+    },
+    {
+      id: 5,
+      title: 'Заплатить за коммунальные услуги',
+      isDone: false,
+      addedAt: '3 сентября',
+      priority: 3,
     },
   ]
+
+  if (tasks === null) {
+    return (
+      <>
+        <h1>Список дел</h1>
+        <div>Загрузка...</div>
+      </>
+    )
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <>
+        <h1>Список дел</h1>
+        <div>Задачи отсутствуют</div>
+      </>
+    )
+  }
+
+  const priorities = ['#fff', '#ffd7b5', '#ffb38a', '#ff9248', '#ff6700']
 
   return (
     <>
       <h1>Список дел</h1>
       <ul>
         {tasks.map((task) => (
-          <li key={task.id}>
+          <li
+            key={task.id}
+            style={{ backgroundColor: `${priorities[task.priority]}` }}
+          >
             <div>
               <strong>Заголовок: </strong>
-              <span>{task.title}</span>
+              <span
+                style={{
+                  textDecorationLine: `${
+                    task.isDone ? 'line-through' : 'none'
+                  }`,
+                }}
+              >
+                {task.title}
+              </span>
             </div>
             <div>
               <strong>Статус: </strong>
