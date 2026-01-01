3b0b64372c8dd4d72d077bd77f8ada53190b7bdb
Sabyrka <sabyrkaga@gmail.com>
2026-01-01 06:33:37 +0500

    feat: initialize project with React, TypeScript, and Vite
    - Add package.json with scripts and dependencies for React and TypeScript.
    - Create App component to display a task list with checkboxes.
    - Add global styles in index.css for a consistent look and feel.
    - Set up main entry point in main.tsx to render the App component.
    - Configure TypeScript with tsconfig files for app and node environments.
    - Add Vite configuration for building and serving the application.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+export const App = () => {
+  const tasks = [
+    { id: 1, title: 'Купить продукты на неделю', isDone: false },
+    { id: 2, title: 'Полить цветы', isDone: true },
+    { id: 3, title: 'Сходить на тренировку', isDone: false },
+  ]
+
+  return (
+    <>
+      <h1>Список дел</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <div>{task.title}</div>
+            <input type="checkbox" checked={task.isDone} readOnly />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
