a9f10085dc625687f450c27ae898cc11fd1af700
Sabyrka <sabyrkaga@gmail.com>
2026-01-02 04:27:55 +0500

    feat: add task details display and improve task selection handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { useEffect, useState } from 'react'
 
+interface TaskDetailsDto {
+  title: string
+  description: string
+  boardTitle: string
+}
+
 interface GlobalTaskListItemDto {
   title: string
   status: number
   priority: number
   addedAt: string
+  boardId: string
+}
+
+interface TaskDetailsData {
+  attributes: TaskDetailsDto
 }
 
 interface GlobalTaskListItemJsonApi {
@@ -14,6 +25,8 @@ interface GlobalTaskListItemJsonApi {
 
 export const App = () => {
   const [selectedTaskId, setSelectedTaskId] = useState<string | null>(null)
+  const [selectedTask, setSelectedTask] = useState<TaskDetailsData | null>(null)
+  const [boardId, setBoardId] = useState<string | null>(null)
   const [tasks, setTasks] = useState<GlobalTaskListItemJsonApi[] | null>(null)
 
   useEffect(() => {
@@ -25,6 +38,21 @@ export const App = () => {
       .then((res) => res.json())
       .then((json) => setTasks(json.data))
   }, [])
+
+  useEffect(() => {
+    if (!boardId || !selectedTaskId) return
+
+    fetch(
+      `https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${selectedTaskId}`,
+      {
+        headers: {
+          'api-key': import.meta.env.VITE_API_KEY,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => setSelectedTask(json.data))
+  }, [boardId, selectedTaskId])
 
   if (tasks === null) {
     return (
@@ -49,50 +77,76 @@ export const App = () => {
   return (
     <>
       <h1>Список дел</h1>
-      <button onClick={() => setSelectedTaskId(null)}>
+      <button
+        onClick={() => {
+          setSelectedTaskId(null)
+          setBoardId(null)
+          setSelectedTask(null)
+        }}
+      >
         Сбросить выделение
       </button>
-      <ul>
-        {tasks.map((task) => (
-          <li
-            key={task.id}
-            style={{
-              backgroundColor: `${priorities[task.attributes.priority]}`,
-              border: `1.5px solid ${
-                task.id === selectedTaskId ? '#646cff' : '#242424'
-              }`,
-            }}
-            onClick={() => setSelectedTaskId(task.id)}
-          >
-            <div>
-              <strong>Заголовок: </strong>
-              <span
-                style={{
-                  textDecorationLine: `${
-                    task.attributes.status === 2 ? 'line-through' : 'none'
-                  }`,
-                }}
-              >
-                {task.attributes.title}
-              </span>
-            </div>
-            <div>
-              <strong>Статус: </strong>
-              <input
-                type="checkbox"
-                checked={task.attributes.status === 2}
-                readOnly
-              />
-            </div>
-            <div>
-              <strong>Дата создания задачи: </strong>
-              <span>
-                {new Date(task.attributes.addedAt).toLocaleDateString()}
-              </span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div style={{ display: 'flex', columnGap: '30px' }}>
+        <ul>
+          {tasks.map((task) => (
+            <li
+              key={task.id}
+              style={{
+                backgroundColor: `${priorities[task.attributes.priority]}`,
+                border: `1.5px solid ${
+                  task.id === selectedTaskId ? '#646cff' : '#242424'
+                }`,
+              }}
+              onClick={() => {
+                setSelectedTaskId(task.id)
+                setBoardId(task.attributes.boardId)
+              }}
+            >
+              <div>
+                <strong>Заголовок: </strong>
+                <span
+                  style={{
+                    textDecorationLine: `${
+                      task.attributes.status === 2 ? 'line-through' : 'none'
+                    }`,
+                  }}
+                >
+                  {task.attributes.title}
+                </span>
+              </div>
+              <div>
+                <strong>Статус: </strong>
+                <input
+                  type="checkbox"
+                  checked={task.attributes.status === 2}
+                  readOnly
+                />
+              </div>
+              <div>
+                <strong>Дата создания задачи: </strong>
+                <span>
+                  {new Date(task.attributes.addedAt).toLocaleDateString()}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <h2>Task details</h2>
+          {!selectedTaskId ? (
+            <p>Task is not selected</p>
+          ) : (
+            <>
+              <p>Title: {selectedTask?.attributes.title}</p>
+              <p>
+                Description:{' '}
+                {selectedTask?.attributes.description || 'No description'}
+              </p>
+              <p>Board Title: {selectedTask?.attributes.boardTitle}</p>
+            </>
+          )}
+        </div>
+      </div>
     </>
   )
 }
